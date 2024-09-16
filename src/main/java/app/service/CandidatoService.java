e637c60a454f8cec49ba19a3ddf9371260da2b33
Maczevski <maczevski@gmail.com>
2024-09-16 20:44:35 -0300

    ╰（‵□′）╯

--- src/main/java/app/service/CandidatoService.java
+++ src/main/java/app/service/CandidatoService.java
@@ -18,7 +18,7 @@ public class CandidatoService {
 	// Cadastra um novo candidato com status ativo
 	public String cadastrarCandidato(Candidato candidato) {
 		
-		if(candidato.getFuncao() != 1 || candidato.getFuncao() != 2) {
+		if(candidato.getFuncao() != 1 && candidato.getFuncao() != 2) {
 			throw new RuntimeException("A função deve ser 1 para prefeito ou 2 para vereador");
 		}
 		
